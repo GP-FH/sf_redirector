b87cba47b438dd041f742e0fd040a25dce6784b5
Marcelo Hudson <marcelo.hudson54@gmail.com>
2017-05-27 21:47:13 +1000

    handle empty body from cin7

--- app.js
+++ app.js
@@ -386,6 +386,9 @@ app.post( '/', function ( req, res ) {
       if ( error ) {
         logger.error( 'Failed to retrieve member_id from Cin7 - reason: ' + error + '. For customer_id: ' + customer_id );
       }
+      else if ( !body ) {
+        logger.warn( 'Failed to retrieve member_id from Cin7 - reason: no results returned for customer_id: ' + customer_id );
+      }
       else if ( body[ 0 ].success == false ) {
         logger.error( 'Failed to retrieve member_id from Cin7 - reason: ' + body[ 0 ].errors[ 0 ] + '. For customer_id: ' + customer_id );
       }
