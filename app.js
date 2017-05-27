ed8b442f3273244b8310ec0737ee8de455a8714d
Marcelo Hudson <marcelo.hudson54@gmail.com>
2017-05-28 01:42:42 +1000

    add missing required fields for sales order update - final touches

--- app.js
+++ app.js
@@ -506,6 +506,9 @@ app.post( '/', function ( req, res ) {
       if ( error ) {
         logger.error( 'Failed to retrieve member_id from Cin7 - reason: ' + error + '. For customer_id: ' + customer_id );
       }
+      else if ( response.statusCode != 200 ) {
+        logger.error( 'Failed to retrieve member_id from Cin7 - status code: ' + response.statusCode + '. For customer_id: ' + customer_id );
+      }
       else if ( body.length == 0 ) {
         logger.error( 'Failed to retrieve member_id from Cin7 - reason: customer does not exist for customer_id: ' + customer_id );
       }
@@ -533,13 +536,16 @@ app.post( '/', function ( req, res ) {
           request( sales_get_options, function ( error, response, body ) {
 
             if ( error ) {
-              logger.error( 'Failed to retrieve sales order ID from Cin7 - reason: ' + error + '. For customer_id: ' + customer_id );
+              logger.error( 'Failed to retrieve sales order ID from Cin7 - reason: ' + error + '. For subscription_id: ' + subscription_id );
+            }
+            else if ( response.statusCode != 200 ) {
+              logger.error( 'Failed to retrieve sales order ID in Cin7 - status code: ' + response.statusCode + '. For subscription_id: ' + subscription_id );
             }
             else if ( body.length == 0 ) {
               logger.error( 'Failed to retrieve sales order ID from Cin7 - reason: sales order does not exist for subscription_id: ' + subscription_id );
             }
             else if ( body[ 0 ].success == false ) {
-              logger.error( 'Failed to retrieve sales order ID from Cin7 - reason: ' + body[ 0 ].errors[ 0 ] + '. For customer_id: ' + customer_id );
+              logger.error( 'Failed to retrieve sales order ID from Cin7 - reason: ' + body[ 0 ].errors[ 0 ] + '. For subscription_id: ' + subscription_id );
             }
             else {
 
@@ -555,7 +561,7 @@ app.post( '/', function ( req, res ) {
                   },
                   body: [ {
                     id: body[ 0 ].id,
-                    internalComments: archetype + ' ' + subscription_id,
+                    internalComments: 'archetype: ' + archetype + ' Subscription_id: ' + subscription_id,
                     currencyCode: 'NZD',
                     taxStatus: 'Incl',
                     taxRate: 0.15
@@ -564,7 +570,6 @@ app.post( '/', function ( req, res ) {
                 };
 
                 request( sales_put_options, function ( error, response, body ) {
-                  logger.info( 'DEBUG: error: ' + JSON.stringify( error ) + ' response:' + JSON.stringify( response ) + ' body: ' + JSON.stringify );
                   if ( error ) {
                     logger.error( 'Failed to update sales order in Cin7 - reason: ' + error + '. For subscription_id: ' + subscription_id );
                   }
