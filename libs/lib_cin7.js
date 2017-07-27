f37f71477f089ffa143f90c8a814e6b2ec454519
Marcelo Hudson <marcelo.hudson54@gmail.com>
2017-07-27 23:26:44 +1000

    queue test - debug logging

--- libs/lib_cin7.js
+++ libs/lib_cin7.js
@@ -44,18 +44,18 @@ var create_sales_order = function ( member_id, plan_id, subscription_id, size_to
 
         if ( error ) {
             logger.info( 'DEBUG: sales order creation error' );
-            callback( error );
+            return callback( error );
         }
         else if ( response.statusCode != 200 ) {
             logger.info( 'DEBUG: sales order creation non 200 resp' );
-            callback( null, {
+            return callback( null, {
                 ok: false,
                 msg: 'status code ' + response.statusCode + ' reason: ' + response.body
             } );
         }
         else {
             logger.info( 'DEBUG: sales order creation looks fine' );
-            callback( null, {
+            return callback( null, {
                 ok: true,
                 fields: body
             } )
