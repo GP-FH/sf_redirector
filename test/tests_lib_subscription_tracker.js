c8cf16f92829a23aae20b2e3e43ee3f4291c0bbe
Marcelo Hudson <marcelo.hudson54@gmail.com>
2017-08-24 22:47:53 +1000

    added tests for weekly counts + switching from monthly to weekly

--- test/tests_lib_subscription_tracker.js
+++ test/tests_lib_subscription_tracker.js
@@ -6,28 +6,222 @@ var expect = require( 'chai' ).expect;
 var nock = require( 'nock' );
 var sub_tracker = require( '../libs/lib_subscription_tracker.js' );
 
-sub_tracker.set( 'test_id', 'test_sub', true );
+sub_tracker.set_weekly( 'test_id_weekly', 'test_sub_id_weekly', true );
+sub_tracker.set_monthly( 'test_id_monthly', 'test_sub_id', true );
+sub_tracker.set_monthly( 'test_id_switch', 'test_sub_id_switch', true );
 
-describe( 'Subscription created to first renewal ', function () {
+describe( 'Subscription created to first renewal - monthly ', function () {
 
-    it( 'first subscription renewal counted', function ( done ) {
-        sub_tracker.increment_and_check_monthly( 'test_id', 'test_sub', function ( err, ret ) {
+    it( '1st subscription renewal counted', function ( done ) {
+        sub_tracker.increment_and_check_monthly( 'test_id_monthly', 'test_sub_id', 'deluxe-box', function ( err, ret ) {
             expect( ret ).to.be.false;
             done();
         }, true );
 
     } );
 
-    it( 'second subscription renewal counted', function ( done ) {
-        sub_tracker.increment_and_check_monthly( 'test_id', 'test_sub', function ( err, ret ) {
+    it( '2nd subscription renewal counted', function ( done ) {
+        sub_tracker.increment_and_check_monthly( 'test_id_monthly', 'test_sub_id', 'deluxe-box', function ( err, ret ) {
             expect( ret ).to.be.false;
             done();
         }, true );
 
     } );
 
-    it( 'third subscription renewal counted - new sales order', function ( done ) {
-        sub_tracker.increment_and_check_monthly( 'test_id', 'test_sub', function ( err, ret ) {
+    it( '3rd subscription renewal counted - new sales order', function ( done ) {
+        sub_tracker.increment_and_check_monthly( 'test_id_monthly', 'test_sub_id', 'deluxe-box', function ( err, ret ) {
+            expect( ret ).to.be.true;
+            done();
+        }, true );
+
+    } );
+
+} );
+
+describe( 'Subscription created to first renewal - weekly ', function () {
+
+    it( '1st subscription renewal counted', function ( done ) {
+        sub_tracker.increment_and_check_weekly( 'test_id_weekly', 'test_sub_id_weekly', 'deluxe-box-weekly', function ( err, ret ) {
+            expect( ret ).to.be.false;
+            done();
+        }, true );
+
+    } );
+
+    it( '2nd subscription renewal counted', function ( done ) {
+        sub_tracker.increment_and_check_weekly( 'test_id_weekly', 'test_sub_id_weekly', 'deluxe-box-weekly', function ( err, ret ) {
+            expect( ret ).to.be.false;
+            done();
+        }, true );
+
+    } );
+
+    it( '3rd subscription renewal counted', function ( done ) {
+        sub_tracker.increment_and_check_weekly( 'test_id_weekly', 'test_sub_id_weekly', 'deluxe-box-weekly', function ( err, ret ) {
+            expect( ret ).to.be.false;
+            done();
+        }, true );
+
+    } );
+
+    it( '4th subscription renewal counted', function ( done ) {
+        sub_tracker.increment_and_check_weekly( 'test_id_weekly', 'test_sub_id_weekly', 'deluxe-box-weekly', function ( err, ret ) {
+            expect( ret ).to.be.false;
+            done();
+        }, true );
+
+    } );
+
+    it( '5th subscription renewal counted', function ( done ) {
+        sub_tracker.increment_and_check_weekly( 'test_id_weekly', 'test_sub_id_weekly', 'deluxe-box-weekly', function ( err, ret ) {
+            expect( ret ).to.be.false;
+            done();
+        }, true );
+
+    } );
+
+    it( '6th subscription renewal counted', function ( done ) {
+        sub_tracker.increment_and_check_weekly( 'test_id_weekly', 'test_sub_id_weekly', 'deluxe-box-weekly', function ( err, ret ) {
+            expect( ret ).to.be.false;
+            done();
+        }, true );
+
+    } );
+
+    it( '7th subscription renewal counted', function ( done ) {
+        sub_tracker.increment_and_check_weekly( 'test_id_weekly', 'test_sub_id_weekly', 'deluxe-box-weekly', function ( err, ret ) {
+            expect( ret ).to.be.false;
+            done();
+        }, true );
+
+    } );
+
+    it( '8th subscription renewal counted', function ( done ) {
+        sub_tracker.increment_and_check_weekly( 'test_id_weekly', 'test_sub_id_weekly', 'deluxe-box-weekly', function ( err, ret ) {
+            expect( ret ).to.be.false;
+            done();
+        }, true );
+
+    } );
+
+    it( '9th subscription renewal counted', function ( done ) {
+        sub_tracker.increment_and_check_weekly( 'test_id_weekly', 'test_sub_id_weekly', 'deluxe-box-weekly', function ( err, ret ) {
+            expect( ret ).to.be.false;
+            done();
+        }, true );
+
+    } );
+
+    it( '10th subscription renewal counted', function ( done ) {
+        sub_tracker.increment_and_check_weekly( 'test_id_weekly', 'test_sub_id_weekly', 'deluxe-box-weekly', function ( err, ret ) {
+            expect( ret ).to.be.false;
+            done();
+        }, true );
+
+    } );
+
+    it( '11th subscription renewal counted', function ( done ) {
+        sub_tracker.increment_and_check_weekly( 'test_id_weekly', 'test_sub_id_weekly', 'deluxe-box-weekly', function ( err, ret ) {
+            expect( ret ).to.be.false;
+            done();
+        }, true );
+
+    } );
+
+    it( '12th subscription renewal counted', function ( done ) {
+        sub_tracker.increment_and_check_weekly( 'test_id_weekly', 'test_sub_id_weekly', 'deluxe-box-weekly', function ( err, ret ) {
+            expect( ret ).to.be.false;
+            done();
+        }, true );
+
+    } );
+
+    it( '13th subscription renewal counted - new sales order created', function ( done ) {
+        sub_tracker.increment_and_check_weekly( 'test_id_weekly', 'test_sub_id_weekly', 'deluxe-box-weekly', function ( err, ret ) {
+            expect( ret ).to.be.true;
+            done();
+        }, true );
+
+    } );
+
+} );
+
+describe( 'Subscription changed from monthly to weekly on renewal changes count to weekly range ', function () {
+
+    it( 'increment count to 10 and return false', function ( done ) {
+        sub_tracker.increment_and_check_weekly( 'test_id_switch', 'test_sub_id_switch', 'deluxe-box-weekly', function ( err, ret ) {
+            expect( ret ).to.be.false;
+            done();
+        }, true );
+
+    } );
+
+    it( 'increment count to 11 and return false', function ( done ) {
+        sub_tracker.increment_and_check_weekly( 'test_id_switch', 'test_sub_id_switch', 'deluxe-box-weekly', function ( err, ret ) {
+            expect( ret ).to.be.false;
+            done();
+        }, true );
+
+    } );
+
+    it( 'increment count to 12 and return false ', function ( done ) {
+        sub_tracker.increment_and_check_weekly( 'test_id_switch', 'test_sub_id_switch', 'deluxe-box-weekly', function ( err, ret ) {
+            expect( ret ).to.be.false;
+            done();
+        }, true );
+
+    } );
+
+    it( 'increment count to 13 and return false ', function ( done ) {
+        sub_tracker.increment_and_check_weekly( 'test_id_switch', 'test_sub_id_switch', 'deluxe-box-weekly', function ( err, ret ) {
+            expect( ret ).to.be.false;
+            done();
+        }, true );
+
+    } );
+
+    it( 'increment count to 14 and return false ', function ( done ) {
+        sub_tracker.increment_and_check_weekly( 'test_id_switch', 'test_sub_id_switch', 'deluxe-box-weekly', function ( err, ret ) {
+            expect( ret ).to.be.false;
+            done();
+        }, true );
+
+    } );
+
+    it( 'increment count to 15 and return false ', function ( done ) {
+        sub_tracker.increment_and_check_weekly( 'test_id_switch', 'test_sub_id_switch', 'deluxe-box-weekly', function ( err, ret ) {
+            expect( ret ).to.be.false;
+            done();
+        }, true );
+
+    } );
+
+    it( 'increment count to 16 and return false ', function ( done ) {
+        sub_tracker.increment_and_check_weekly( 'test_id_switch', 'test_sub_id_switch', 'deluxe-box-weekly', function ( err, ret ) {
+            expect( ret ).to.be.false;
+            done();
+        }, true );
+
+    } );
+
+    it( 'increment count to 17 and return false ', function ( done ) {
+        sub_tracker.increment_and_check_weekly( 'test_id_switch', 'test_sub_id_switch', 'deluxe-box-weekly', function ( err, ret ) {
+            expect( ret ).to.be.false;
+            done();
+        }, true );
+
+    } );
+
+    it( 'reset count to 5 and return false ', function ( done ) {
+        sub_tracker.increment_and_check_weekly( 'test_id_switch', 'test_sub_id_switch', 'deluxe-box-weekly', function ( err, ret ) {
+            expect( ret ).to.be.false;
+            done();
+        }, true );
+
+    } );
+
+    it( 'increment count to 6 and return true - new sales order created', function ( done ) {
+        sub_tracker.increment_and_check_weekly( 'test_id_switch', 'test_sub_id_switch', 'deluxe-box-weekly', function ( err, ret ) {
             expect( ret ).to.be.true;
             done();
         }, true );
