57cadc52ab800d017732b28695ec1f30643737f6
Marcelo Hudson <marcelo.hudson54@gmail.com>
2017-11-13 23:00:21 +1100

    some newbies

---
+++ libs/lib_order.js
@@ -0,0 +1,54 @@
+var chargebee_coupon = require( './lib_chargebee_coupon.js' );
+var chargebee = require( './lib_chargebee.js' );
+var cin7 = require( './lib_cin7.js' );
+var slack = require( './lib_slack.js' );
+
+var order_create_new_subscription = ( sub, coupons ) => {
+    var customer = '';
+    var subscription = ';'
+
+    return new Promise( ( resolve, reject ) => {
+        chargebee_coupon.chargebee_coupon_create_new( process.env.FRIEND_REFERRAL_CODE_ID, process.env.FRIEND_REFERRAL_SET_NAME, sub.customer_id )
+            .then( ( ret ) => {
+                return chargebee.chargebee_get_customer_info( sub.customer_id );
+            } )
+            .then( ( ret ) => {
+                customer = ret.customer;
+                return cin7.cin7_get_customer_record( customer.email )
+            } )
+            .then( ( ret ) => {
+                if ( !ret.exists ) {
+                    return chargebee.chargebee_get_subscription_info( sub.id );
+                }
+                else if ( ret.exists ) {
+                    return cin7.cin7_create_sales_order( ret.member_id, sub.plan, sub.id, sub.cf_topsize, sub.cf_bottomsize, 'NOT_SET' );
+                }
+            } )
+            .then( ( ret ) => {
+                if ( ret.resolve ) {
+                    return resolve();
+                }
+
+                subscription = ret.subscription;
+                return cin7.cin7_create_customer_record( customer, subscription );
+            } )
+            .then( ( ret ) => {
+                cin7.cin7_create_sales_order( ret.member_id, sub.plan, sub.id, sub.cf_topsize, sub.cf_bottomsize, 'NOT_SET' );
+            } )
+            .then( ( ret ) => {
+                if ( coupons ) {
+                    return chargebee_coupon.chargebee_coupon_check_referral( coupons[ 0 ].entity_id );
+                }
+
+                return resolve();
+            } )
+            .then( ( ret ) => {
+                return resolve();
+            } )
+            .catch( ( err ) => {
+                reject( err );
+            } );
+    } );
+};
+
+exports.order_create_new_subscription = order_create_new_subscription;
