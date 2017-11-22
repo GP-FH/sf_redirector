53b4df90e34a65e41864f06a653b642dde6ac8a1
Marcelo Hudson <marcelo.hudson54@gmail.com>
2017-11-22 20:10:04 +1100

    add autopilot list moves

--- routes/sub_hook.js
+++ routes/sub_hook.js
@@ -41,21 +41,21 @@ router.post( '/', function ( req, res, next ) {
         product_plan.product_plan_is_one_off( plan_id )
             .then( ( ret ) => {
                 if ( ret.one_off ) {
-                    /*
-                     *  Move them from the completers list to the Gift Box Purchasers list in Autopilot
-                     */
-
                     if ( process.env.ENVIRONMENT == 'prod' ) {
+                        /*
+                         *  Move them from the completers list to the Gift Box Purchasers list in Autopilot
+                         */
+
                         autopilot.autopilot_move_contact_to_new_list( 'contactlist_AAB1C098-225D-48B7-9FBA-0C4A68779072', 'contactlist_E427B712-F86E-4864-80F5-C8C5AC335E17', email );
                     }
                     return order.order_create_new_purchase( req.body.content.subscription );
                 }
                 else {
-                    /*
-                     *  Move them from the completers list to the subscribers list in Autopilot
-                     */
-
                     if ( process.env.ENVIRONMENT == 'prod' ) {
+                        /*
+                         *  Move them from the completers list to the subscribers list in Autopilot
+                         */
+
                         autopilot.autopilot_move_contact_to_new_list( 'contactlist_AAB1C098-225D-48B7-9FBA-0C4A68779072', 'contactlist_1C4F1411-4376-4FEC-8B63-3ADA5FF4EBBD', email );
                     }
 
