c4a083b45c95a55206b1b410a2af6aa9ba4c4a5f
Marcelo Hudson <marcelo.hudson54@gmail.com>
2017-11-22 20:55:42 +1100

    MORON

--- libs/lib_product_plan.js
+++ libs/lib_product_plan.js
@@ -3,7 +3,7 @@ var product_plans_subscriptions = [ 'premium-box', 'deluxe-box', 'premium-box-we
 
 var product_plan_is_one_off = ( plan_id ) => {
     return new Promise( ( resolve, reject ) => {
-        if ( product_plan_one_offs.includes( plan_id ) ) {
+        if ( product_plans_one_offs.includes( plan_id ) ) {
             logger.info( 'DEBUG: product plan: one_off = true' );
             return resolve( {
                 ok: true,
