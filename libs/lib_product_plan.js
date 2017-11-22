fcd5a341676057096417e4c2a6a071e657c5d58f
Marcelo Hudson <marcelo.hudson54@gmail.com>
2017-11-22 21:02:47 +1100

    MORON logging

--- libs/lib_product_plan.js
+++ libs/lib_product_plan.js
@@ -3,6 +3,7 @@ var product_plans_subscriptions = [ 'premium-box', 'deluxe-box', 'premium-box-we
 
 var product_plan_is_one_off = ( plan_id ) => {
     return new Promise( ( resolve, reject ) => {
+        logger.info( 'DEBUG: product_plans_one_offs.include(plan_id) = ' + product_plans_one_offs.includes( plan_id ) );
         if ( product_plans_one_offs.includes( plan_id ) ) {
             logger.info( 'DEBUG: product plan: one_off = true' );
             return resolve( {
