73f2579b669ec706bda5723ff918daae7ddf6ece
Marcelo Hudson <marcelo.hudson54@gmail.com>
2018-02-22 20:59:38 +1100

    add new logger file

---
+++ libs/lib_logger.js
@@ -0,0 +1,29 @@
+/*
+ *
+ *  lib_logger: this is the logging service. Allows logs of differing severity levels (info, warn, error)
+ *  to be sent to paper trail for viewing/searching through. Also gracefully logs and handles exceptions
+ *  without killing the service.
+ *
+ */
+
+const winston = require("winston");
+const papertrail = require("winston-papertrail").Papertrail;
+
+const winstonPapertrail = new winston.transports.Papertrail( {
+  host: 'logs5.papertrailapp.com',
+  port: 41600,
+  handleExceptions: true,
+  humanReadableUnhandledException: true,
+  logFormat: function ( level, message ) {
+    return '<<<' + level + '>>> ' + message;
+  }
+} );
+
+const logger = new winston.Logger( {
+  transports: [
+    winstonPapertrail
+  ],
+  exitOnError: false
+} );
+
+exports.logger = logger;
