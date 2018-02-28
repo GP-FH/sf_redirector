1286f57b9d57ba0c91e60c20dd3f9edbd5c5b60f
Marcelo Hudson <marcelo.hudson54@gmail.com>
2018-02-28 22:37:21 +1100

    WHAT'SA HAPPENIN'? DEBUGLIN' MORE, MAKE CODE BETTERRER

--- libs/lib_subscription_tracker.js
+++ libs/lib_subscription_tracker.js
@@ -113,7 +113,7 @@ const increment_and_check_monthly = async ( customer_id, subscription_id, plan_i
       if ( err ) {
         throw new VError(err);
       }
-
+      console.log("INCREMENT!");
       //  if reply is 4, reset the counter to 1
       if ( reply == 4 ) { //TODO: this no work no idea why yet
         console.log(`customer id : ${customer_id}. subscription_id : ${subscription_id}`);
