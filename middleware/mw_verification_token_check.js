f6ae2cd054e4e22693571e47c28eaa42c5a2f53a
Marcelo Hudson <marcelo.hudson54@gmail.com>
2018-03-06 13:22:08 +1100

    verificastion token middleware

--- middleware/mw_verification_token_check.js
+++ middleware/mw_verification_token_check.js
@@ -1,5 +1,6 @@
 const verification_token_check = (req, res, next) => {
   if ( req.query.token == process.env.VERIFICATION_TOKEN ) {
+    console.log("token check successful");
     next();
   }else{
     res.status( 403 ).send();
