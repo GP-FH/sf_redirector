8984367de3fe15d40dd65c0a7350d7d8e7015328
Marcelo Hudson <marcelo.hudson54@gmail.com>
2018-03-06 19:27:46 +1100

    small middleware comment addition

--- middleware/mw_verification_token_check.js
+++ middleware/mw_verification_token_check.js
@@ -1,3 +1,10 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * mw_verification_token_check.js: this middleware function checks all incoming requests for a valid
+ * verification token. No token, no access.
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 const verification_token_check = (req, res, next) => {
   if ( req.query.token == process.env.VERIFICATION_TOKEN ) {
     next();
