4b66778153e5161c479bf22ef96e03fb192dd915
Marcelo Hudson <marcelo.hudson54@gmail.com>
2018-03-18 09:19:16 +1100

    add health check endpoint fix

--- routes/health_check.js
+++ routes/health_check.js
@@ -1,6 +1,6 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *
- * This route handles DO Load Balancer health checks
+ * This route handles DO Load Balancer health checks (only in prod)
  * For settings see sf-lb-001 load balancer in DigitalOcean
  *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
@@ -8,8 +8,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get( '/', async function ( req, res, next) {
-  res.status( 200 ).send();
+router.get( '/', function ( req, res, next) {
+  res.status( 200 ).send('ok');
 });
 
 module.exports = router;
