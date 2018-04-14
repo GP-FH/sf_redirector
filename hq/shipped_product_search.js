ba1c3d93c13daa616c9a41fac8bb95e3bc719ee8
Marcelo Hudson <marcelo.hudson54@gmail.com>
2018-04-14 23:36:30 +1000

    switching to fat arrows where I should have before

--- hq/shipped_product_search.js
+++ hq/shipped_product_search.js
@@ -1,17 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-/*router.get( '/', async function ( req, res, next) {
-  // render shipped product search
-  res.status(200).send("shipped_product_search");
-} );*/
-
-router.route('/').get(async (req,res) => {
+router.route('/').get( async (req,res) => {
     res.send("shipped_product_search");
 });
 
 // error handling for the sub route
-router.use( function ( err, req, res, next ) {
+router.use( ( err, req, res, next ) => {
   res.end();
   logger.error( JSON.stringify( err ) );
 } );
--- routes/hq.js
+++ routes/hq.js
@@ -3,29 +3,29 @@ const router = express.Router();
 
 const shipped_product_search = require('../hq/shipped_product_search');
 
-/* * *
+/* * * * * * * * * * * * * *
  * Put your HQ routes here
- * * */
+ * * * * * * * * * * * * * */
 router.use('/shipped_product_search', shipped_product_search);
 
-router.get( '/', async function ( req, res, next) {
+router.get( '/', async ( req, res, next) => {
   //render hq home
   res.status(200).send("hq_home");
 } );
 
-router.get( '/login', async function ( req, res, next) {
+router.get( '/login', async ( req, res, next) => {
   // render hq_login
 } );
 
 /*
  * handle 404
  */
-router.use(function (req, res, next) {
+router.use((req, res, next) => {
   res.status(404).send("NOT FOUND");
 })
 
 // error handling for the sub route
-router.use( function ( err, req, res, next ) {
+router.use( ( err, req, res, next ) => {
   res.end();
   logger.error( JSON.stringify( err ) );
 } );
