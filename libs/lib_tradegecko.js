43f8084884527a5c20302667757c24cdb63a87cf
Marcelo Hudson <marcelo.hudson54@gmail.com>
2018-04-23 21:51:33 +1000

    some more logging

--- libs/lib_tradegecko.js
+++ libs/lib_tradegecko.js
@@ -165,6 +165,7 @@ const tradegecko_create_company = async (customer, company_type) => {
 const tradegecko_create_sales_order_contact = async (subscription, customer) => {
   try{
     const ret1 = await _tradegecko_create_company("consumer", customer.email, `${customer.first_name} ${customer.last_name}`, customer.phone);
+    logger.info(`customer id: ${ret1.customer.id}. shipping address: ${subscription.shipping_address}`);
     const ret2 = await _tradegecko_create_address(ret1.company.id, subscription.shipping_address);
 
     return {ok:true, company:ret1.company, address:ret2.address};
