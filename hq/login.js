914d78633e01fc5102e9c3438a1cd58ee6e44d88
Marcelo Hudson <marcelo.hudson54@gmail.com>
2018-04-30 22:45:55 +1000

    add new files

---
+++ hq/login.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+router.route('/').get( async (req,res) => {
+  res.status(200).send("login");
+});
+
+// error handling for the sub route
+router.use( ( err, req, res, next ) => {
+  res.end();
+  logger.error( JSON.stringify( err ) );
+} );
+
+module.exports = router;
