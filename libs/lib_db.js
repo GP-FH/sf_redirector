a7545adc76a732364cc846d641aed10d35fe0ad6
Marcelo Hudson <marcelo.hudson54@gmail.com>
2018-05-03 22:11:58 +1000

    you are a dummy

--- libs/lib_db.js
+++ libs/lib_db.js
@@ -12,13 +12,12 @@ const pool = mysql.createPool( {
 const find_user_by_name = async (username) => {
   const connection = await pool.getConnection();
   let [rows, fields] = await connection.execute(`select username, pw from hq_users where username = '${username}'`);
+  connection.release();
 
-  if (rows.length == 0){
-    //connection.release();
-    return {ok:true, user:false};
-  }
+  if (rows.length == 0) return {ok:true, user:false};
+  logger.info(`returned from DB: ${rows}`);
+
   const user = {username:username, password: rows[0].pw};
-  //connection.release();
 
   return {ok:true, user:user};
 };
