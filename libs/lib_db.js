22912dd4f64c16b479fba964d8ff15edfe069229
Marcelo Hudson <marcelo.hudson54@gmail.com>
2018-05-03 21:47:30 +1000

    passport promise jamming + spme logging

--- libs/lib_db.js
+++ libs/lib_db.js
@@ -1,3 +1,4 @@
+const logger = require('../libs/lib_logger');
 const mysql = require('mysql2/promise');
 const pool = mysql.createPool( {
     connectionLimit: 500,
@@ -9,14 +10,12 @@ const pool = mysql.createPool( {
 } );
 
 const find_user_by_name = async (username) => {
+  logger.info("Made it to the DB function");
   const connection = await pool.getConnection();
-
-  let [rows, fields] = await connection.execute(`select username, password from hq_users where username = '${username}'`);
-
+  let [rows, fields] = await connection.execute(`select username, pw from hq_users where username = '${username}'`);
   connection.release();
 
   if (row.length == 0) return {ok:true, user:false};
-
   const user = {username:username, password: row[0].pw};
 
   return {ok:true, user:user};
