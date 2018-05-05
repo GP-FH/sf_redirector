002eac25cc944f3cd9a23b5c741a86ea22122359
Marcelo Hudson <marcelo.hudson54@gmail.com>
2018-05-05 22:38:20 +1000

    beginnings of the piping between BE and FE

--- public/javascript/fe_shipped_product_searcher.js
+++ public/javascript/fe_shipped_product_searcher.js
@@ -1,9 +1,7 @@
-const logger = require("../../libs/lib_logger");
-
 function sps_check(){
-  logger.info(`hit sps_check client js function: ${document.getElementById("email_sku_check_form")}`);
+  console.log(`hit sps_check client js function: ${document.getElementById("email_sku_check_form")}`);
 }
 
 function sps_get(){
-  logger.info(`hit sps_get client js function: ${document.getElementById("email_get_form")}`);
+  console.log(`hit sps_get client js function: ${document.getElementById("email_get_form")}`);
 }
