1b7996415781ee5713340ab5f57fae408d74ecd3
Marcelo Hudson <marcelo.hudson54@gmail.com>
2018-05-05 22:47:00 +1000

    beginnings of the piping between BE and FE

--- public/javascript/fe_shipped_product_searcher.js
+++ public/javascript/fe_shipped_product_searcher.js
@@ -1,7 +1,7 @@
 function sps_check(){
-  ${document.getElementById("email_sku_check_form").submit();
+  document.getElementById("email_sku_check_form").submit();
 }
 
 function sps_get(){
-  ${document.getElementById("email_get_form").submit();
+  document.getElementById("email_get_form").submit();
 }
