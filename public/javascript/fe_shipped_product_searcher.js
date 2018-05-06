2d9759eca79304ef08f071ea14ac509b0468ae5e
Marcelo Hudson <marcelo.hudson54@gmail.com>
2018-05-06 22:29:52 +1000

    beginnings of the piping between BE and FE

--- public/javascript/fe_shipped_product_searcher.js
+++ public/javascript/fe_shipped_product_searcher.js
@@ -1,16 +1,16 @@
 $(document).ready(() => {
   $("#email_get_form").submit((event) => {
     event.preventDefault();
-    const data = $("#email_get_form :input");//.serializeArray();
-    console.log(data[0].value);
-    /*$.get('/api/sps_check', document.getElementById("email_sku_check_form"), (data, status) => {
+    const data = $("#email_get_form :input");
+
+    $.get('/api/sps_get', {"email":data[0].value} document.getElementById("email_sku_check_form"), (data, status) => {
       console.log(JSON.stringify(data));
-    });*/
+    });
   });
 
   $("#email_sku_check_form").submit((event) => {
     console.log(JSON.stringify(event));
-    /*$.get('/api/sps_get', document.getElementById("email_get_form"), (data, status) => {
+    /*$.get('/api/sps_check', document.getElementById("email_get_form"), (data, status) => {
       console.log(JSON.stringify(data));
     });*/
   });
