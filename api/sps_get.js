65c9272e41f09368e55af9513584d96d7142d257
Marcelo Hudson <marcelo.hudson54@gmail.com>
2018-05-06 22:52:15 +1000

    beginnings of the piping between BE and FE

--- api/sps_get.js
+++ api/sps_get.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const logger = require("../libs/lib_logger");
+const sps = require("../libs/lib_shipped_product_search");
 
 router.route('/').get(async (req,res) => {
   logger.info(`Received by /sbs_get: ${req.query.email}`);
-  ret = sps.sps_get(req.query.email);
+  ret = await sps.sps_get(req.query.email);
   res.send(ret);
 });
 
