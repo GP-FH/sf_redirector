f750e0c3fd969370a5b8f2c96166917faca193dd
Marcelo Hudson <marcelo.hudson54@gmail.com>
2018-07-08 22:36:05 +1000

    product_search tests

---
+++ libs/lib_product_search.js
@@ -0,0 +1,6 @@
+/*
+ *
+ * The goal for this lib is to provide the foundations for a more flexible product search. 
+ *
+ *
+ */
---
+++ public/javascript/fe_product_search.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+  $( "#target" ).click(function() {
+    alert( "Handler for .click() called." );
+  });
+
+  //for (let i = 0; i < 50; i++ ){
+    //$("#results").append(`<div class="col-lg-3 col-md-4 col-xs-6"><a href="#" class="d-block mb-4 h-100"><img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt=""></a></div>`);
+  //}
+});
