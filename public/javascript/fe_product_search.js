0567af81f0763fd2df32c5e6dd4775670f4e7eec
Marcelo Hudson <marcelo.hudson54@gmail.com>
2018-07-09 06:45:05 +1000

    product_search tests more testing

--- public/javascript/fe_product_search.js
+++ public/javascript/fe_product_search.js
@@ -1,5 +1,8 @@
 $(document).ready(() => {
-  $( "#test_button" ).click(function() {
+  $( "#sub_search_input" ).submit((event) => {
+    event.preventDefault();
+    const data = $("#sub_search_input :input");
+
     for (let i = 0; i < 50; i++ ){
       $("#results").append(`<div class="col-lg-3 col-md-4 col-xs-6"><a href="#" class="d-block mb-4 h-100"><img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt=""></a></div>`);
     }
