535231f2ba8b85ab3648227956e6751a1983660d
Marcelo Hudson <marcelo.hudson54@gmail.com>
2018-07-09 07:05:17 +1000

    product_search tests more testing

--- public/javascript/fe_product_search.js
+++ public/javascript/fe_product_search.js
@@ -8,8 +8,3 @@ $(document).ready(() => {
     }
   });
 });
-
-<figure class="figure">
-  <img src="..." class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
-  <figcaption class="figure-caption">A caption for the above image.</figcaption>
-</figure>
