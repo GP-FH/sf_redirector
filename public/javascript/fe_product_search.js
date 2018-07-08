a9ae581d4b2993c83bf24c13db02a96a64944eaa
Marcelo Hudson <marcelo.hudson54@gmail.com>
2018-07-09 06:57:06 +1000

    product_search tests more testing

--- public/javascript/fe_product_search.js
+++ public/javascript/fe_product_search.js
@@ -4,7 +4,7 @@ $(document).ready(() => {
     const data = $("#sub_search_input :input");
 
     for (let i = 0; i < 50; i++ ){
-      $("#results").append(`<div class="col-lg-3 col-md-4 col-xs-6"><a href="#" class="d-block mb-4 h-100"><img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt=""></a></div>`);
+      $("#results").append(`<div class="col-lg-3 col-md-4 col-xs-6"><a href="#" class="d-block mb-4 h-100"><img class="img-fluid img-thumbnail" src="https://d1yx6mil86g02p.cloudfront.net/uploads/variant_image/image/16709483/medium_RYU-SFX-Sae.jpg" alt=""></a><span>[SKU]: [BRAND] [PRODUCT_NAME] [COLOUR] [SIZE]</span></div>`);
     }
   });
 });
