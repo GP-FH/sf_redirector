23cadd55475091e83db82154d75a6653a14e5c59
Marcelo Hudson <marcelo.hudson54@gmail.com>
2018-07-17 22:42:23 +1000

    profile hook sub storage

--- routes/profile_hook.js
+++ routes/profile_hook.js
@@ -105,7 +105,7 @@ async function _transform_request_for_storage (qs, keen, palette){
     email: qs.email,
     archetype: 'not_yet_defined',
     gender: qs.gender,
-    childname: qs.hername || qs.hisname,,
+    childname: qs.hername || qs.hisname,
     childage: qs.sheage || qs.heage,
     topsize: qs.shetopsize || qs.hetopsize,
     bottomsize: qs.shebottomsize || qs.hebottomsize,
