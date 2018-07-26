f401fcf02938a7add8228e44594e91fa3f93a9f0
Marcelo Hudson <marcelo.hudson54@gmail.com>
2018-07-26 22:41:49 +1000

    added tests + new slack send function

--- test/tests_lib_chargebee.js
+++ test/tests_lib_chargebee.js
@@ -2,19 +2,24 @@ const expect = require( 'chai' ).expect;
 const chargebee = require('../libs/lib_chargebee');
 const VError = require("verror");
 
-describe( 'Subscription created to first renewal - monthly ', () => {
-  it( '1st subscription renewal counted', async () => {
-    let ret = await sub_tracker.increment_and_check_monthly( 'test_id_monthly', 'test_sub_id', 'deluxe-box', true );
-    expect( ret ).to.be.false;
+describe( 'Pause chargebee subscription - ', () => {
+  it( 'missing subscription_id parameter should throw error', async () => {
+    try{
+      await chargebee.chargebee_pause_subscription();
+      expect(false).to.be.true;
+    }
+    catch(error){
+      expect(error.message).to.equal('chargebee_pause_subscription() called with undefined subscription_id parameter');
+    }
   } );
 
-  it( '2nd subscription renewal counted', async () => {
-    let ret = await sub_tracker.increment_and_check_monthly( 'test_id_monthly', 'test_sub_id', 'deluxe-box', true );
-    expect( ret ).to.be.false;
-  } );
-
-  it( '3rd subscription renewal counted', async () => {
-    let ret = await sub_tracker.increment_and_check_monthly( 'test_id_monthly', 'test_sub_id', 'deluxe-box', true );
-    expect( ret ).to.be.true;
+  it( 'null subscription_id parameter should throw error', async () => {
+    try{
+      await chargebee.chargebee_pause_subscription(null);
+      expect(false).to.be.true;
+    }
+    catch(error){
+      expect(error.message).to.equal('chargebee_pause_subscription() called with undefined subscription_id parameter');
+    }
   } );
 } );
