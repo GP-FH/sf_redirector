835560a0a5718871fc9c4bce501c235d66485017
Marcelo Hudson <marcelo.hudson54@gmail.com>
2018-08-01 22:12:44 +1000

    Changing tests to run using Jest instead of Mocha

--- tests/lib_order.test.js
+++ tests/lib_order.test.js
@@ -1,16 +1,15 @@
-const expect = require( 'chai' ).expect;
 const order = require('../libs/lib_order');
 
 describe( 'Validate whether new subscription is for existing customer', () => {
-  it( 'customer is new', async () => {
+  test('customer is new', async () => {
     const sub = {name: 'Gary', phone: '5555555555', cf_firstmame:'baby'};
     const ret = await order.order_validate_if_for_new_customer(sub);
-    expect( ret ).to.be.true;
-  } );
+    expect( ret ).toBe(true);
+  });
 
-  it( 'customer is existing', async () => {
+  test('customer is existing', async () => {
     const sub = {name: 'Gary', phone: '5555555555'};
     const ret = await order.order_validate_if_for_new_customer(sub);
-    expect( ret ).to.be.false;
-  } );
+    expect( ret ).toBe(false);
+  });
 } );
