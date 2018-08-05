33281b278d3accc22f86daf5c763d5887e57e97c
Marcelo Hudson <marcelo.hudson54@gmail.com>
2018-08-05 23:08:41 +1000

    asd

--- hq/product_search.js
+++ hq/product_search.js
@@ -4,11 +4,11 @@ const logger = require("../libs/lib_logger");
 const connect = require('connect-ensure-login');
 const product_search = require("../libs/lib_product_search");
 
-router.route('/').get(connect.ensureLoggedIn('/hq/login'), async (req, res, next) => {
+router.route('/').get(connect.ensureLoggedIn('/hq/login'), async (req, res) => {
   res.render('product_search', {csrfToken: req.csrfToken()});
 });
 
-router.route('/').post(async (req, res) => {
+router.route('/').post(async (req, res, next) => {
   logger.info(`HERE IS THE FORM? ${JSON.stringify(req.body, null, 4)}`);
 
   const test_obj = [
