7b80ccef4c527b31fea721872a9c7274976f52c9
Marcelo Hudson <marcelo.hudson54@gmail.com>
2018-08-17 22:07:47 +1000

    asdf

--- libs/lib_tradegecko.js
+++ libs/lib_tradegecko.js
@@ -285,7 +285,7 @@ const tradegecko_get_images = async (filters={}, storage=[], page=1) => {
 
   concat_storage = storage.concat(res.body.images);
   const pagination_info = JSON.parse(res.headers["x-pagination"]);
-  logger.info(`RL LIMIT LEFT ${JSON.parse(res.headers["X-Rate-Limit-Remaining"])}`);
+  logger.info(`RL LIMIT LEFT ${JSON.stringify(res.headers["X-Rate-Limit-Remaining"])}`);
 
   /*
    * If it's a multi-page result make the recursive call to get the rest of the
