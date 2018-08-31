c87548e0e9f04e592aad60766c5efd86defc279b
Marcelo Hudson <marcelo.hudson54@gmail.com>
2018-08-31 23:16:58 +1000

    missing async

--- libs/lib_product_search.js
+++ libs/lib_product_search.js
@@ -455,7 +455,6 @@ async function _remove_sent_variants (products, variants, line_items){
   for (let i = 0; i < products.length; i++){
     for (let j = 0; j < line_item_variants.length; j++){
       if (products[i].variant_ids.includes(line_item_variants[j])){
-        logger.info('hello?');
         all_product_variants.push(products[i].variant_ids);
       }
     }
@@ -477,7 +476,7 @@ async function _remove_sent_variants (products, variants, line_items){
   return variants;
 }
 
-function _extract_line_item_variants (line_items){
+async function _extract_line_item_variants (line_items){
   let line_item_variants = [];
 
   for (let i = 0; i < line_items; i++){
