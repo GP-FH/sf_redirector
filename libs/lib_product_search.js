1c187a0d8ad4e1274bb213d09615cd875cc9d372
Marcelo Hudson <marcelo.hudson54@gmail.com>
2018-09-28 21:02:06 +1000

    sq

--- libs/lib_product_search.js
+++ libs/lib_product_search.js
@@ -322,7 +322,7 @@ async function _create_results_array (products, variants, images){
     o['id'] = variants[i].id;
     o['sku'] = variants[i].sku;
     o['name'] = variants[i].product_name;
-    o['stock_on_hand'] = variants[i].stock_on_hand - variants[i].committed_stock;
+    o['stock_on_hand'] = (variants[i].stock_on_hand - variants[i].committed_stock);
     o['price'] = `$${variants[i].wholesale_price}`;
     o['colour'] = variants[i].opt1;
     o['size'] = variants[i].opt2;
