ae991f345a5584970e4f76b6d5465139666325ad
Marcelo Hudson <marcelo.hudson54@gmail.com>
2018-09-28 22:27:51 +1000

    asd

--- libs/lib_product_search.js
+++ libs/lib_product_search.js
@@ -502,11 +502,11 @@ async function _remove_sent_variants (products, variants, line_items){
   
   //TODO: remove all matching variants from the variants array and return
   for (let i = 0; i < variants.length; i++){
+    if (variants[i].id == 40975379){
+      logger.info(`Previously ordered variant is in the variant list`);
+    }
     for (let j = 0; j < all_product_variants.length; j++){
       if(all_product_variants[j].includes(variants[i].id)){
-        if (variants[i].id == 40975379){
-          logger.info(`Previously ordered variant found. Splicing!`);
-        }
         variants.splice(i, 1);
       }
     }
